0ab995f811ddc6ffe865dd533f9898f03c283a17
brownlee <brownlee@adobe.com>
2013-10-12 21:34:01 -0700

    magic will now clone your shit

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,7 @@
 /*global module:false*/
 module.exports = function(grunt) {
 
+  // WHAT HAPPEN:
   // prompt for git user name
   // prompt for 'your theme'
 
@@ -16,36 +17,56 @@ module.exports = function(grunt) {
   // Project configuration.
   grunt.initConfig({
 
-    prompt:  {
-      deets: {
+    prompt:  { // Yes, this will nest four levels deep. Be afraid.
+      gitname: {
         options: {
           questions: [
             {
               config: 'setup.gitname', 
               type: 'input', 
               message: 'Enter your github username (used to build a clone URL):',
-              // default: 'your_git_username', 
-              validate: function(value) { return (value) ? true : "no really, enter your user name"}
-            },
+              validate: function(value) { 
+                return (value) ? true : "no really, enter your user name";
+              }
+            }       
+          ]
+        }
+      },
+      
+      repo: { // asking this in a separate object so I can use the first answer in filter
+        options: {
+          questions: [
             {
-              config: 'setup.themename', 
+              config: 'gitclone.theme.options.repository', 
               type: 'input', 
               message: 'Enter your theme name (also used to build a clone URL):',
-              // default: 'your_git_username', 
-              validate: function(value) { return (value) ? true : "uh... that's not a theme name"}
+              validate: function (value) { 
+                return (value) ? true : "uh... that's not a theme name";
+              },
+              filter: function (value) {
+                var repoUrl = 'http://github.com/' 
+                repoUrl += grunt.config('setup.gitname');
+                repoUrl += '/';
+                repoUrl += value;
+                repoUrl += '.git';
+                console.log('repoUrl', repoUrl);
+                return repoUrl;
+              }
             }
           ]
         }
       }
-    }
+    }  // /prompt <-- contains 'prom', which none of us went to
+    
    
   });
 
   // These plugins provide necessary tasks.
   grunt.loadNpmTasks('grunt-prompt');
+  grunt.loadNpmTasks('grunt-git');
   grunt.loadNpmTasks('grunt-contrib-copy');
 
   // Default task.
-  grunt.registerTask('default', ['prompt', '']);
+  grunt.registerTask('default', ['prompt', 'gitclone']);
 
 };
